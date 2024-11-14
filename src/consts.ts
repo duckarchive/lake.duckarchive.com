8f21b855c6f99079cfff29bbe91fbb50ba86d280
alexandrtovmach <alexandrtovmach@gmail.com>
2024-11-14 16:44:32 +0100

    feat: Cleared content

--- src/consts.ts
+++ src/consts.ts
@@ -2,60 +2,53 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Base Page Metadata, src/layouts/BaseLayout.astro
-export const BRAND_NAME = "AstroVerse";
-export const SITE_TITLE = "AstroVerse";
-export const SITE_DESCRIPTION = "An Astro Theme for Visual Storytelling";
+export const BRAND_NAME = "Архівна Качка";
+export const SITE_TITLE = "Озеро";
+export const SITE_DESCRIPTION =
+  "Статті про генеалогію, історію та інструменти Качиного Всесвіту";
 
 // Tags Page Metadata, src/pages/tags/index.astro
-export const Tags_TITLE = "AstroVerse - All Tags";
-export const Tags_DESCRIPTION =
-  "AstroVerse - All tags and the count of articles related to each tag";
+export const Tags_TITLE = "#теги";
+export const Tags_DESCRIPTION = "Теги статей, які ви знайдете на сайті";
 
 // Tags Page Metadata, src/pages/tags/[tag]/[page].astro
 export function getTagMetadata(tag: string) {
   return {
-    title: `All articles on '${tag}' tag in AstroVerse`,
-    description: `Explore articles about ${tag} for different perspectives and in-depth analysis.`,
+    title: `Всі статті в Озері по тегу '${tag}'`,
+    description: `Знайдіть статті по тегу ${tag}.`,
   };
 }
 
 // Category Page Metadata, src/pages/category/[category]/[page].astro
 export function getCategoryMetadata(category: string) {
   return {
-    title: `All articles in '${category}' category in AstroVerse`,
-    description: `Browse all articles under the ${category} category in AstroVerse`,
+    title: `Всі статті в Озері по категорії '${category}'`,
+    description: `Знайдіть статті по категорії ${category}`,
   };
 }
 
 // Header Links, src/components/Header.astro
 export const HeaderLinks = [
-  { href: "/category/One/1/", title: "One" },
-  { href: "/category/Two/1/", title: "Two" },
-  { href: "/category/Three/1/", title: "Three" },
 ];
 
 // Footer Links, src/components/Footer.astro
 export const FooterLinks = [
-  { href: "/posts/why-astro/", title: "Astro" },
-  { href: "/posts/tailwind-typography/", title: "Tailwind" },
-  { href: "/tags/", title: "Tags" },
+  { href: "/tags/", title: Tags_TITLE },
+  // { href: "/category/official/", title: "Качині Історії" },
+  // { href: "/category/public/", title: "Від підписників" },
+  // { href: "/category/other/", title: "Різне" },
 ];
 
 // Social Links, src/components/Footer.astro
 export const SocialLinks = [
   { href: "/rss.xml", icon: "tabler:rss", label: "RSS" },
   {
-    href: "https://twitter.com/inotelab",
-    icon: "tabler:brand-twitter",
-    label: "Twitter",
-  },
-  {
-    href: "https://github.com/inotelab",
-    icon: "tabler:brand-github",
-    label: "GitHub",
+    href: "https://t.me/spravnakachka",
+    icon: "tabler:brand-telegram",
+    label: "Telegram",
   },
 ];
 
 // Search Page Metadata, src/pages/search.astro
-export const SEARCH_PAGE_TITLE = `${SITE_TITLE} - Site Search`;
-export const SEARCH_PAGE_DESCRIPTION = `Search all content on ${SITE_TITLE}`;
+export const SEARCH_PAGE_TITLE = `${SITE_TITLE} | Пошук`;
+export const SEARCH_PAGE_DESCRIPTION = `Шукайте статті в ${SITE_TITLE}`;
