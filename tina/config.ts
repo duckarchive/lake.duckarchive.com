0b6b4ba59585d8ef7f22cdd175e186a5c33a49aa
alexandrtovmach <alexandrtovmach@gmail.com>
2024-11-18 19:16:06 +0100

    feat: Added tinacms cloud

--- tina/config.ts
+++ tina/config.ts
@@ -1,17 +1,13 @@
 import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
+const branch = process.env.GITHUB_BRANCH || "main";
 
 export default defineConfig({
   branch,
 
   // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+  clientId: process.env.PUBLIC_TINA_CLIENT_ID,
   // Get this from tina.io
   token: process.env.TINA_TOKEN,
 
